1cd9d9e2b695467e2586c711cae77089f87ff502
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-04-07 18:26:37 -0500

    Wire up the search bar to the ytisearch and add delay on search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -20,8 +21,12 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
+        
+        this.videoSearch('star wars');
+    }
 
-        YTSearch({key: API_KEY, term: 'star wars'}, (videos) =>{
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term}, (videos) =>{
             this.setState({
                   videos,
                  selectedVideo: videos[0] 
@@ -29,9 +34,12 @@ class App extends Component {
         });
     }
 
+
     render (){
+
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (<div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
