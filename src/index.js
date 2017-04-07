aaaea2f8910b0af1d3a51e0248ba0d3941a7e47a
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-04-06 20:26:23 -0500

    add YT search

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBtntlGogvGoj3gDakSD2FryL2hIGNngdY';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, (data) =>{
+    console.log(data);
+});
+
 // create new component. this component should produce some 
 //HTML
-const App = () => {
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (data) =>{
+            this.setState({ videos:data });
+        });
+    }
+
+    render (){
     return (<div>
                 <SearchBar />
             </div>);
-}
+    }
+};
 
 
 //Take this component's generated html and put it on the page
